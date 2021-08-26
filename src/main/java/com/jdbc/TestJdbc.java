3cb32e01bf894347c9665d4259e8a0307495d54e
mohamed085 <mohamed085555@gmail.com>
2021-08-26 18:34:45 +0200

    first commit

---
+++ src/main/java/com/jdbc/TestJdbc.java
@@ -0,0 +1,32 @@
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+
+		String jdbcUrl = "jdbc:mysql://localhost:3306/demo?useSSL=false";
+		String user = "root";
+		String pass = "root";
+		
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			
+			Connection myConn =
+					DriverManager.getConnection(jdbcUrl, user, pass);
+						
+			System.out.println("Connection successful!!!");
+			
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		
+	}
+
+}
+
+
+
